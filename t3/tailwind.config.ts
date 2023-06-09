cb24731963aebfe2a82034cd663b6e3ca60abb11
gdevcodes <gprog@tutanota.com>
2023-06-09 17:38:01 +0200

    añadido códigos de colores daisyUI

--- t3/tailwind.config.ts
+++ t3/tailwind.config.ts
@@ -5,5 +5,17 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "rbg(104 50 243)",
+          secondary: "rgb(54, 57, 68)",
+          accent: "rgb(221, 84, 113)",
+          "base-100": "rgb(255, 255, 255)",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 } satisfies Config;
