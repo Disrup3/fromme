c0a82ed3bd24925eff94f431fa0db2ec0b71fbca
Pablo González Sánchez <pgonzalezs1999@gmail.com>
2023-06-25 14:12:27 +0200

    Tarjetas terminadas

--- t3/src/components/ui/NFTcard.tsx
+++ t3/src/components/ui/NFTcard.tsx
@@ -11,30 +11,33 @@ const heartIconStyle = {
     color: "red",
 }
 
-const imageStyle = {
-    backgroundImage: "url('images/carousel-test/1.png')",
-    width: "100%",
-}
-
 const NFTcard: FC<Props> = ({ item }) => {
     
-    const [cardHovered, setCardHovered] = useState<boolean>(false);
-    
-    const handleMouseOver = () => {
-        setCardHovered(true);
-    }
-
-    const handleMouseOut = () => {
-        setCardHovered(false);
-    }
+    const [isHovered, setIsHovered] = useState<boolean>(false);
 
     return (
         <div className="container max-w-sm rounded-xl overflow-hidden shadow-lg mb-10
             w-[100%] sm:w-[45%] xl:w-[30%]"
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
+            onMouseOver={ () => setIsHovered(true) }
+            onMouseOut={ () => setIsHovered(false) }
         >
-            <div className={`${imageStyle}`}><p>Hola</p></div>
+            <div className="relative overflow-hidden">
+                <img className="" src={item.image} alt={item.title} style={{
+                    transform: isHovered ? "scale(1.1)" : "scale(1)",
+                    transition: "transform 0.5s",
+                }} />
+                {isHovered && (
+                    <div className="absolute top-0 left-0 w-full h-full flex flex-wrap justify-center items-center bg-black bg-opacity-50">
+                        <div className="flex w-full justify-around items-center">
+                            <button className="btn btn-primary">{item.price.toFixed(2)} EUR</button>
+                            <button className="bg-white p-5 rounded-full w-8 h-8 flex justify-center items-center">
+                                <FontAwesomeIcon icon={faHeart} />
+                            </button>
+                        </div>
+                        <button className="btn btn-primary">Purchase now</button>
+                    </div>
+                )}
+            </div>
             <div className="px-6 py-4">
                 <p className="font-bold text-xl mb-2">{item.title}</p>
                 <div className="flex justify-between">
@@ -52,8 +55,7 @@ const NFTcard: FC<Props> = ({ item }) => {
                 <div className="divider h-[1px]"></div>
                 <p>{item.stock} in stock</p>
             </div>
-            { cardHovered && <p>Hovereado!!!!</p>}
         </div>
-  );
+    );
 };
 export default NFTcard;
